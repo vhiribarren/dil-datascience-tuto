605df522e4a3f20c36b503242788edb1e9fb24a8
vhiribarren <vhiribarren@users.noreply.github.com>
2016-10-09 15:42:22 +0200

    Cleaning with pandas

--- part2-kaggle/decision_tree_gender_age.py
+++ part2-kaggle/decision_tree_gender_age.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 from sklearn import tree
 import numpy as np
+import pylab
 
 
 DATASET_TRAINING = "./titanic-dataset/train.csv"
@@ -35,28 +36,35 @@ def save_result(data):
 
 
 def predict(csv_train, csv_test):
-    # Training
-    data_train = np.array(csv_train)
-    data_test = np.array(csv_test)
-    # Change from label to numeric value
-    data_train[data_train == 'male'] = 0
-    data_train[data_train == 'female'] = 1
-    data_test[data_test == 'male'] = 0
-    data_test[data_test == 'female'] = 1
-    # Some age are missing, putting an arbitrary value
-    data_train[data_train[0::,TRAIN_INDEX_AGE] == '', TRAIN_INDEX_AGE] = 30
-    data_test[data_test[0::,TEST_INDEX_AGE] == '', TEST_INDEX_AGE] = 30
 
-    #print(data_train[0::,[TRAIN_INDEX_PASSENGER_ID, TRAIN_INDEX_GENRE, TRAIN_INDEX_AGE]])
-    survival = data_train[0::,TRAIN_INDEX_SURVIVED]
-    train = data_train[0::,[TRAIN_INDEX_GENRE, TRAIN_INDEX_AGE]].astype(np.float)
-    test =  data_test[0::,[TEST_INDEX_GENRE, TEST_INDEX_AGE]].astype(np.float)
+    df_train = pd.read_csv(DATASET_TRAINING, header=0)
+    df_test = pd.read_csv(DATASET_TEST, header=0)
+    df_survived = df_train['Survived']
+    df_test_id = df_test['PassengerId']
+
+    for df in [df_train, df_test]:
+        # Change from label to numeric value
+        df['S_Gender'] = df["Sex"].map(lambda x: 0 if x == "male" else 1)
+        # Fill missing values in age
+        median_age = df_train["Age"].median()
+        df['S_AgeFill'] = df["Age"]
+        df.loc[ df['Age'].isnull(), 'S_AgeFill'] =  median_age
+        # Add new feature that tells when age was missing
+        df['S_AgeNull'] = pd.isnull(df['Age']).astype(int)
+        #print(df.describe())
+
+    df_train = df_train[['S_Gender', 'S_AgeFill', 'S_AgeNull']]
+    df_test = df_test[['S_Gender', 'S_AgeFill', 'S_AgeNull']]
+    survival = df_survived.values
+    train = df_train.values
+    test = df_test.values
 
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(train, survival)
     result = clf.predict(test)
-    result = np.stack((data_test[0::, TEST_INDEX_PASSENGER_ID], result), axis=-1)
-    #print(result)
+    id = df_test_id.values
+    result = np.stack((id, result), axis=-1)
+    print(result)
 
     return result
 
